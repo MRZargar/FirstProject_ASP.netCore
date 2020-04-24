eeda66f4a2b5b8e980021089950fed95f4da045f
MRZargar <zargar.mr.k@gmail.com>
2020-04-25 02:11:00 +0430

    create a ASP.net Core MVC project and add it to solution using command: cd /home/zargar/Desktop/Mehr dotnet "new" "mvc" "-lang" "C#" "-n" "Mehr" "-o" "Mehr" cd /home/zargar/Desktop/Mehr dotnet "sln" "/home/zargar/Desktop/Mehr/Mehr.sln" "add" "/home/zargar/Desktop/Mehr/Mehr/Mehr.csproj"

---
+++ Mehr/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mehr.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
