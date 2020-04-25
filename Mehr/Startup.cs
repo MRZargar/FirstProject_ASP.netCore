114c86f89f884a7fcde21b45f1529272a5eae099
MRZargar <zargar.mr.k@gmail.com>
2020-04-25 05:34:35 +0430

    Add Area and set Routing

--- Mehr/Startup.cs
+++ Mehr/Startup.cs
@@ -54,6 +54,11 @@ namespace Mehr
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapAreaControllerRoute(
+                    name: "AppArea",
+                    areaName: "App",
+                    pattern: "App/{controller=Home}/{action=Index}/{id?}");
+                    
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
