6f80133faa838e4a2ca95c38745883420376f508
MRZargar <zargar.mr.k@gmail.com>
2020-05-04 02:28:16 +0430

    update database using commands: cd /home/zargar/Desktop/Mehr dotnet "build" "/home/zargar/Desktop/Mehr/Mehr.sln"
    cd /home/zargar/Desktop/Mehr/DataLayer
    dotnet ef migrations add changeColumns --startup-project ../Mehr/Mehr.csproj
    dotnet ef database update changeColumns --startup-project ../Mehr/Mehr.csproj
    
    cd /home/zargar/Desktop/Mehr
    dotnet "build" "/home/zargar/Desktop/Mehr/Mehr.sln"

--- DataLayer/Migrations/20200503214403_changeColumns.Designer.cs
+++ DataLayer/Migrations/20200503214403_changeColumns.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using DataLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace DataLayer.Migrations
 {
     [DbContext(typeof(MyContext))]
-    partial class MyContextModelSnapshot : ModelSnapshot
+    [Migration("20200503214403_changeColumns")]
+    partial class changeColumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DataLayer/Migrations/20200503214403_changeColumns.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataLayer.Migrations
+{
+    public partial class changeColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isValid",
+                table: "Sponsors");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isValid",
+                table: "SponsorTransactions",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isMale",
+                table: "Sponsors",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "picName",
+                table: "Sponsors",
+                maxLength: 500,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "color",
+                table: "Colleagues",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isMale",
+                table: "Colleagues",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "picName",
+                table: "Colleagues",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isValid",
+                table: "SponsorTransactions");
+
+            migrationBuilder.DropColumn(
+                name: "isMale",
+                table: "Sponsors");
+
+            migrationBuilder.DropColumn(
+                name: "picName",
+                table: "Sponsors");
+
+            migrationBuilder.DropColumn(
+                name: "color",
+                table: "Colleagues");
+
+            migrationBuilder.DropColumn(
+                name: "isMale",
+                table: "Colleagues");
+
+            migrationBuilder.DropColumn(
+                name: "picName",
+                table: "Colleagues");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isValid",
+                table: "Sponsors",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
