00e4486ef2866d5a01216f6e9d6993cefc248d1b
MRZargar <zargar.mr.k@gmail.com>
2020-05-13 12:23:48 +0430

    Test

--- Mehr/wwwroot/js/pages/dashboard-lite.js
+++ Mehr/wwwroot/js/pages/dashboard-lite.js
@@ -16,11 +16,8 @@
     *************************************************/
 
     var lineGradientChart1 = new Chartist.Line('#gradient-line-chart1', {
-        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-        series: [
-            [125, 200, 125, 225, 175, 275, 220],
-            [175, 275, 165, 280, 120, 226, 150]
-        ]
+        labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        series: [10,1, 0, 1, 0,10, 1,10,63,10,10  ,10]
     }, {
             low: 100,
             fullWidth: true,
