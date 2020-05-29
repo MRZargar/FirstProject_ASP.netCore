bdcc25ed50152bba9ae010fc18588751c2029c6f
MRZargar <zargar.mr.k@gmail.com>
2020-05-29 17:06:51 +0430

    Date

--- Mehr/Classes/Extention.cs
+++ Mehr/Classes/Extention.cs
@@ -1,13 +1,38 @@
 ﻿using DataLayer;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Globalization;
 
 namespace Mehr.Classes
 {
-    public static class Extention {
+    public static class Extention 
+    {
         public static void SetViewMessage(this Controller controller, string message, WebMessageType type,
                 bool WithCloseBtn = true)
         {
             controller.TempData["Message"] = WebMessage.Get(message, type, WithCloseBtn);
+        }
+
+        public static string ToSolar(this DateTime value)
+        {
+            PersianCalendar pc = new PersianCalendar();
+            return pc.GetYear(value) + "/" + pc.GetMonth(value).ToString("00") + "/" +
+                   pc.GetDayOfMonth(value).ToString("00");
+        }
+
+        public static string ToSolar(this string value)
+        {
+            return Convert.ToDateTime(value).ToSolar();
+        }
+
+        public static string ToAD(this string value)
+        {
+            PersianCalendar pc = new PersianCalendar();
+            var dateSplit = value.Split('/');
+            int year = Convert.ToInt32(dateSplit[0]);
+            int month = Convert.ToInt32(dateSplit[1]);
+            int day = Convert.ToInt32(dateSplit[2]);
+            return pc.ToDateTime(year, month, day, 0, 0, 0, 0).ToShortDateString();
         }
     }
 }
